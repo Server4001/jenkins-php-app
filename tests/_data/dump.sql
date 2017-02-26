dc04ba605628fdc1e0336eabed8434dc2b1e02a8
Brice Bentler <server4001@gmail.com>
2017-02-26 09:10:10 -0800

    Adds generated Codeception integration tests files

--- tests/_data/dump.sql
+++ tests/_data/dump.sql
@@ -1,1 +1,12 @@
-/* Replace this file with actual dump of your database */
+use jenkinsphp_test;
+
+CREATE TABLE IF NOT EXISTS dogs (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `breed` VARCHAR(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `IDX_NAME` (`name`),
+  INDEX `IDX_BREED` (`breed`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
