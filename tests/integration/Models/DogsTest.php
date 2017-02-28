738efefa96df8f578124c6e9b749576c62a29fb1
Brice Bentler <server4001@gmail.com>
2017-02-27 18:36:41 -0800

    Adds example of integration tests using codeception

--- tests/integration/Models/DogsTest.php
+++ tests/integration/Models/DogsTest.php
@@ -2,6 +2,7 @@
 
 namespace Models;
 
+use BentlerDesign\Models\Dogs;
 use Codeception\Test\Unit;
 
 class DogsTest extends Unit
@@ -11,17 +12,58 @@ class DogsTest extends Unit
      */
     protected $tester;
 
+    /**
+     * @var null|\PDO
+     */
+    private $pdo = null;
+
+    /**
+     * @var null|Dogs
+     */
+    private $dogsModel = null;
+
     protected function _before()
     {
+        $this->pdo = $this->getModule('Db')->dbh;
+        $this->dogsModel = new Dogs($this->pdo);
     }
 
     protected function _after()
     {
+        $this->dogsModel = null;
+        $this->pdo = null;
     }
 
-    // tests
-    public function testMe()
+    public function testUpdateDogReturnsTrueWhenThereAreNoUpdatesPresent()
     {
+        $results = $this->dogsModel->updateDog(0, []);
 
+        $this->assertTrue($results);
+    }
+
+    public function testGetDogReturnsDogRecord()
+    {
+        $name = 'Fido';
+        $breed = 'Mutt';
+
+        // Create a dog record.
+        $affected = $this->pdo->exec("INSERT INTO dogs SET `name` = '{$name}', breed = '{$breed}';");
+        $this->assertSame(1, $affected);
+
+        $id = (int)$this->pdo->lastInsertId();
+
+        // Call the getDog() model method, and assert results.
+        $dog = $this->dogsModel->getDog($id);
+
+        $this->assertInternalType('array', $dog);
+        $this->assertArrayHasKey('id', $dog);
+        $this->assertArrayHasKey('name', $dog);
+        $this->assertArrayHasKey('breed', $dog);
+        $this->assertArrayHasKey('created_at', $dog);
+        $this->assertArrayHasKey('updated_at', $dog);
+
+        $this->assertEquals($id, $dog['id']);
+        $this->assertEquals($name, $dog['name']);
+        $this->assertEquals($breed, $dog['breed']);
     }
 }
